0562144cacfb9b0f557df87a08abefb2a469bc85
ElmubakiWataara <elmubaki@gmail.com>
2026-02-04 14:25:11 +0000

    fixed model import path sensitive 2nd

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,4 +1,4 @@
-const Task = require("../models/task");
+const Task = require("../models/Task");
 
 exports.getTasks = async (req, res) => {
   const tasks = await Task.find({ userId: req.user.id });
